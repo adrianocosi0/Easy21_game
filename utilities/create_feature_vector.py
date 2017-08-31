df3c078b24ab8f98978e039d86f0f0a590ed1fa6
Adriano Cosi <adriano.cosi@digitalshadows.com>
2017-08-31 18:12:34 +0100

    small modification in creating features

--- utilities/create_feature_vector.py
+++ utilities/create_feature_vector.py
@@ -4,22 +4,6 @@ from utilities.special_functions import cartesian_product
 ind_range=[range(0,10,3), range(4,13,3)]
 cols_range=[range(0,16,3),range(6,22,3)]
 
-def create_feature_vector_matrix(s,a,ind_range=ind_range, cols_range=cols_range):
-    initial_mat = np.zeros(36)
-    ind_matches=[]
-    col_matches=[]
-    for el,ser,cont in zip(s,[ind_range,cols_range],[ind_matches,col_matches]):
-        for ind,z in enumerate(zip(*ser)):
-            n,m=z
-            if el in range(n,m):
-                cont += [ind+1]
-    if a == 1:
-        res = 17+np.multiply.outer(ind_matches, col_matches)
-    else:
-        res = np.multiply.outer(ind_matches, col_matches)-1
-    initial_mat[res]=1
-    return initial_mat
-
 def create_feature_vector(s,a,ind_range=ind_range, cols_range=cols_range):
     initial_mat = np.zeros((3,6,2))
     ind_matches=[]
